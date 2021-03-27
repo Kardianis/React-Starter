ca21550be867eb0ea8cec4b842e57addbab0067b
Kardianis <michal_kuszneruk@tlen.pl>
2021-03-27 23:59:05 +0100

    main

---
+++ src/components/Column/Column.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Column extends React.Component{
+render () {
+    return (
+        <section className={styles.component}>
+            <h3 className={styles.title}></h3>
+        </section>
+    )
+}
+}
+
+export default Column;
